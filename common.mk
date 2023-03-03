36a093b24a918daf74c2dc4f44cdae6c9bdacbbd
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    mt6789-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -198,6 +198,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.transsion
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.1.vendor \
