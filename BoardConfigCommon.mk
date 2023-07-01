4763aa45d6ca0e5fe275bb4389a5645b843e12b8
bengris32 <bengris32@protonmail.ch>
2023-07-01 17:14:30 +0700

    mt6789-common: Define Bootloader and Platform flags
    Change-Id: I8a4aad74ec90124cc35847c64d4db23f97af7ad7
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,14 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := x6833b
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6789
+BOARD_HAS_MTK_HARDWARE := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/properties/vendor.prop
