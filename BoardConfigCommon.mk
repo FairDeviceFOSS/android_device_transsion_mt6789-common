7dd21bbee1c2b187d2c18d62ff5ff1cac10a8d35
Christian Oder <myself5@carbonrom.org>
2023-07-01 17:20:17 +0700

    mt6789-common: Build disabled VBMeta image
    Change-Id: I48e3ff106a2e4e044ed39b86a94f3bc80b93f530
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,5 +80,9 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.mt6789
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Inherit proprietary blobs
 include vendor/transsion/mt6789-common/BoardConfigVendor.mk
