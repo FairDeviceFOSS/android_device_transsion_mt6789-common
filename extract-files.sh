e369e618895f41ed827c9e84c8c48cb17ad4c95c
LuK1337 <priv.luk@gmail.com>
2023-10-29 16:41:03 +0000

    mt6789-common: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -72,6 +72,10 @@ function blob_fixup() {
             "$PATCHELF" --replace-needed "libbinder.so" "libbinder-v32.so" "$2"
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
+        vendor/lib*/libwvhidl.so|\
+        vendor/lib*/mediadrm/libwvdrmengine.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
         vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|\
         vendor/lib64/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so|\
         vendor/lib64/mt6789/libaalservice.so|\
