d5045ede9b61525c5b7f7f21d69f8973a6bba373
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-09 21:10:30 +0700

    mt6789-common: rootdir: Cleanup non-existance and unused service import
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -76,10 +76,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Init files
 PRODUCT_PACKAGES += \
-    factory_init.connectivity.common.rc \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
     fstab.mt6789 \
     fstab.mt6789.vendor_ramdisk \
     init_connectivity.rc \
@@ -96,13 +92,6 @@ PRODUCT_PACKAGES += \
     init.project.rc \
     init.recovery.usb.rc \
     init.sensor_2_0.rc \
-    meta_init.connectivity.common.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    meta_init.vendor.rc \
-    multi_init.rc \
     ueventd.mt6789.rc
 
 # Project ID Quota
