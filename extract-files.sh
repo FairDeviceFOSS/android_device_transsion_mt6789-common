f590f0f553d12246a2483b83036f9bdf50dab39f
ChrisCatto <chriscatto08@gmail.com>
2024-06-22 16:29:50 +0200

    mt6789-common: Link mediatek camera provider against v32 `libutils`
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,7 @@ function blob_fixup() {
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
         vendor/bin/hw/mt6789/camerahalserver |\
+        vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so |\
         vendor/lib*/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
