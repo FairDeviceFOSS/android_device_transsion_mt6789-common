42699da7433ff0b68f435d83d2b74473002e52d8
fjrXTR <fajarslebew31@gmail.com>
2024-07-07 19:00:21 +0000

    mt6789-common: Build stub mtkpower service
    Change-Id: I6284e6f8a1d6ce7660502a0b553d0fa9824d6703
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- common.mk
+++ common.mk
@@ -286,6 +286,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-V2-ndk.vendor \
+    android.hardware.power@1.3.vendor 
+
+PRODUCT_PACKAGES += \
+    vendor.mediatek.hardware.mtkpower@1.2-service.stub
+
 # Power | Dummy mtkperf lib
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
