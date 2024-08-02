ba74ac9d3a29aab9c6c73bf10313f6519cb08e43
fjrXTR <fajarslebew31@gmail.com>
2024-08-02 12:31:42 +0000

    mt6789-common: Move `BOOTLOADER` flags to device tree & And Add more device on `setup-makefiles`
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "LH7n X6833B"
+write_headers "X6837 X6833B X676C"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
