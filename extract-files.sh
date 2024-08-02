888ae2c3ebc31545938aa9b7ad84844f7e5cfb56
fjrXTR <fajarslebew31@gmail.com>
2024-08-02 13:33:07 +0000

    mt6789-common: Fix missmatch
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,15 +62,15 @@ function blob_fixup() {
     case "$1" in
         vendor/bin/hw/android.hardware.gnss-service.mediatek |\
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
-            "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
             ;;
         vendor/bin/hw/mt6789/camerahalserver |\
         vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so |\
         vendor/lib64/hw/mt6789/android.hardware.camera.provider@2.6-impl-mediatek.so |\
-        vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service |\
-            "$PATCHELF" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "$2"
-            "$PATCHELF" --replace-needed "libbinder.so" "libbinder-v32.so" "$2"
-            "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+        vendor/bin/hw/vendor.mediatek.hardware.pq@2.2-service)
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib*/libwvhidl.so|\
         vendor/lib*/mediadrm/libwvdrmengine.so)
@@ -93,7 +93,7 @@ function blob_fixup() {
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
         system_ext/lib64/libsink.so)
-            "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
+            "${PATCHELF}" --add-needed "libshim_sink.so" "${2}"
             ;;
     esac
 }
