9efe147f8947dd8cd87d6ac00b4166c3bd1123c1
fjrXTR <fajarslebew31@gmail.com>
2024-08-02 23:00:26 +0000

    mt6789-common: Switch to AIDL vibrator service
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- common.mk
+++ common.mk
@@ -395,6 +395,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
+    
 # VNDK 
 PRODUCT_PACKAGES += \
     libstagefright_foundation-v33 \
