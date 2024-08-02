1f14d35541ff397e5c3a01e91d8b5924686e7025
fjrXTR <fajarslebew31@gmail.com>
2024-08-02 11:29:51 +0000

    mt6789-common: Drop touch & vibrator service * Move vibrator service to device tree
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- common.mk
+++ common.mk
@@ -357,19 +357,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
-
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.transsion
-
+    
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.transsion
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
