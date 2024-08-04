3aad28ae24cd3ecb6b8ad05a3ae53fe23af68360
fjrXTR <fajarslebew31@gmail.com>
2024-08-04 09:38:57 +0000

    mt6789-common: Build `MtkInCallService`
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- common.mk
+++ common.mk
@@ -76,6 +76,9 @@ PRODUCT_PACKAGES += \
     libhapticgenerator \
     libprocessgroup.vendor
 
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
