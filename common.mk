024ce8c60c7b886d0785c1a7a365b8396d695c2b
fjrXTR <fajarslebew31@gmail.com>
2024-08-17 13:58:09 +0000

    mt6789-common: Build `vibrator` service to device tree * not all devices have the same vibrator hardware
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- common.mk
+++ common.mk
@@ -435,10 +435,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mediatek
     
 # VNDK 
 PRODUCT_PACKAGES += \
